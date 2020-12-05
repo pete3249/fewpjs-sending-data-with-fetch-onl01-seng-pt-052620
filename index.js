7eb9610fdceea096dc8ec7e54b4329eeb871f34b
pete3249 <katelynrpeterson16@gmail.com>
2020-12-05 12:51:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })    
+    .then(function(object) {
+        let newUser = object;
+        const p = document.createElement('p');
+        p.innerHTML = object.id;
+        document.body.appendChild(p);
+    })
+    .catch(function(error) {
+        const pError = document.createElement('p');
+        pError.innerHTML = error.message
+        document.body.appendChild(pError);
+    }) 
+}
+
+submitData("Kate", "peterson@gmail.com")
